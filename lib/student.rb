2ab20be06df042235185caa23e62a411764e3132
Christopher Wood <cjwood032@gmail.com>
2018-07-12 17:59:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -85,7 +85,7 @@ def self.first_student_in_grade_10
   sql = "SELECT * FROM students WHERE grade = 10 ORDER BY students.id LIMIT 1"
   DB[:conn].execute(sql).map do |row|
     self.new_from_db(row)
-  end
+  end.first
 end
 ###################
   def self.drop_table
